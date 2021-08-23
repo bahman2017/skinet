ac0480ef91b474bc17031c04628c97e4a216262f
Bahman Masarrat <bmasarrat@gmail.com>
2021-08-22 20:04:25 -0700

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using Core.Entities;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public StoreContext Context { get; }
+
+        public ProductsController(StoreContext context)
+        {
+            Context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await Context.Products.ToListAsync();
+            return Ok(products);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await Context.Products.FindAsync(id);
+        }
+    }
+}
